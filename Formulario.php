26d007ec6fc63d4148f2ab09cca87f72e4710593
Javier Omar Andrade Martínez <72424458+LonzoVitch@users.noreply.github.com>
2023-10-10 22:54:17 -0600

    Add files via upload

---
+++ Formulario.php
@@ -0,0 +1,15 @@
+<form action="form_concatena.php" method="post">
+    <legend>FORMULARIO EN PHP</legend>
+
+
+<h1> Nombre </h1>
+<input name= "txtnombre">
+
+<h1> Primer Apellido </h1>
+<input name = "textapellido">
+
+
+
+<button>Enviar</button>
+
+</form>
